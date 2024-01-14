e0a3836d0eb3d7e472bbfc8950b73c29cbe165b4
Евгений Марков <evgenijmarkov@iMac-Evgenij.local>
2024-01-14 19:22:09 +0300

    contacts page

---
+++ components/contacts-item/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="contacts__item">
+      <h2 class="title">{{ title }}</h2>
+      <h3 class="subtitle">Телефон: </h3>
+      <a 
+          class="link" 
+          href="tel:{{ phone }}">
+          {{ phone }}
+      </a>
+      <h3 class="subtitle">Электронная почта: </h3>
+      <a 
+          class="link" 
+          href="mailto:{{ email }}">
+          {{ email }}
+      </a>
+  </div>
+</template>
+
+<script
+  setup
+  lang="ts"
+>
+interface Props {
+  title: string;
+  phone: string;
+  email: string;
+}
+
+defineProps<Props>();
+</script>
+
+<style
+  scoped
+  lang="scss"
+>
+.contacts__item {
+  display: flex;
+  flex-direction: column;
+}
+.title {
+  margin-bottom: 20px;
+}
+
+.link {
+  display: block;
+
+  &:not(:last-child) {
+    margin-bottom: 10px;
+  }
+}
+</style>
